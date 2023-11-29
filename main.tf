b5d76fa58f8cecee5836f2af24bfaface2c58171
InsiemeAnnina <147087670+InsiemeAnnina@users.noreply.github.com>
2023-11-29 19:44:19 +0100

    feat: terraform

---
+++ main.tf
@@ -0,0 +1,22 @@
+resource "google_storage_bucket" "static_site" {
+  name          = "bestbook"
+  location      = "EU"
+  force_destroy = true
+
+    website {
+        main_page_suffix = "index.html"
+        not_found_page   = "404.html"
+    }
+}
+
+resource "google_storage_bucket_object" "picture" {
+  name   = "italiana-picture"
+  source = "author.jpg"
+  bucket = "bestbook"
+}
+
+resource "google_storage_bucket_object" "index" {
+  name   = "italiana-catalozza"
+  source = "index.html"
+  bucket = "bestbook"
+}
---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "google" {
+  project = "calm-nation-401021"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
