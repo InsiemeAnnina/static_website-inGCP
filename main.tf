5f66ca83a41647954ae9823fb7b5b045b9b8c47d
InsiemeAnnina <147087670+InsiemeAnnina@users.noreply.github.com>
2023-11-29 20:45:55 +0100

    feat: update main.tf

--- main.tf
+++ main.tf
@@ -3,20 +3,20 @@ resource "google_storage_bucket" "static_site" {
   location      = "EU"
   force_destroy = true
 
-    website {
-        main_page_suffix = "index.html"
-        not_found_page   = "404.html"
-    }
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
 }
 
 resource "google_storage_bucket_object" "picture" {
   name   = "italiana-picture"
   source = "author.jpg"
-  bucket = "bestbook"
+  bucket = google_storage_bucket.static_site.name
 }
 
 resource "google_storage_bucket_object" "index" {
   name   = "italiana-catalozza"
   source = "index.html"
-  bucket = "bestbook"
+  bucket = google_storage_bucket.static_site.name
 }
